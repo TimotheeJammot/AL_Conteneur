5220f3158f4c702329fc551290e11c443fe3cc29
TimotheeJammot <tim.jammot@yahoo.fr>
2017-02-03 15:43:00 +0100

    AcquisitionImpl ajouté

---
+++ src/impl/AcquisitionImpl.java
@@ -0,0 +1,48 @@
+package impl;
+
+import java.util.Set;
+
+import services.Acquisition;
+import services.AcquisitionFailure;
+import services.OCComponent;
+import services.OCService;
+
+public class AcquisitionImpl implements Acquisition {
+	
+	@Override
+	public Set<OCComponent> getNewComponents() throws AcquisitionFailure {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Set<OCComponent> getDisappearedComponents() throws AcquisitionFailure {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Set<OCComponent> getCurrentComponents() throws AcquisitionFailure {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Set<OCService> getNewServices() throws AcquisitionFailure {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Set<OCService> getDisappearedServices() throws AcquisitionFailure {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Set<OCService> getCurrentServices() throws AcquisitionFailure {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
