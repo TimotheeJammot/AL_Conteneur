619ebad759ab36538e3530f407582fb25af00a71
TimotheeJammot <tim.jammot@yahoo.fr>
2017-02-03 15:52:31 +0100

    Implémentations 2

---
+++ src/impl/JavaComponent.java
@@ -0,0 +1,28 @@
+package impl;
+
+import java.util.ArrayList;
+
+import services.OCComponent;
+import services.OCService;
+
+public class JavaComponent extends OCComponent {
+
+	@Override
+	public ArrayList<OCService> getRequiredServices() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<OCService> getProvidedServices() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getComponentProperties() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/impl/JavaService.java
@@ -0,0 +1,19 @@
+package impl;
+
+import services.OCService;
+
+public class JavaService extends OCService {
+
+	@Override
+	public String getServiceName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getServiceProperties() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
