541ab7882f8b97af611a6e7019517ea4132a2369
Sackboy31 <kevin.machu@hotmail.fr>
2017-02-10 17:49:51 +0100

    Implementation constructeur

--- src/main/java/impl/JavaComponent.java
+++ src/main/java/impl/JavaComponent.java
@@ -1,21 +1,46 @@
 package impl;
 
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Properties;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import services.OCComponent;
 import services.OCService;
 
 public class JavaComponent extends OCComponent {
+	public JavaComponent(Properties componantProperties) {
+		properties = componantProperties;
+        required = new ArrayList<OCService>();
+        provided = new ArrayList<OCService>();
+
+        JsonParser jp = new JsonParser();
+        JsonArray ja = jp.parse(componantProperties.getProperty("services")).getAsJsonArray();
+        Iterator<JsonElement> ijo = ja.iterator();
+        while(ijo.hasNext()){
+            JsonObject serviceJson = ijo.next().getAsJsonObject();
+            JavaService javaService = new JavaService(serviceJson);
+            if (serviceJson.get("type").getAsString().equals("required")){
+                required.add(javaService);
+            }else if (serviceJson.get("type").getAsString().equals("provided"){
+                provided.add(javaService);
+            }
+        }
+
+	}
 
 	@Override
 	public ArrayList<OCService> getRequiredServices() {
-		
+
 		return required;
 	}
 
 	@Override
 	public ArrayList<OCService> getProvidedServices() {
-		
+
 		return provided;
 	}
 
--- src/main/java/impl/JavaService.java
+++ src/main/java/impl/JavaService.java
@@ -1,8 +1,18 @@
 package impl;
-
+import com.google.gson.JsonObject;
 import services.OCService;
 
+import java.util.Properties;
+
 public class JavaService extends OCService {
+	public JavaService(JsonObject serviceJson) {
+		properties = new Properties();
+		name = serviceJson.get("nom").getAsString();
+		properties.setProperty("type",serviceJson.get("type").getAsString());
+		properties.setProperty("lien",serviceJson.get("lien").getAsString());
+		properties.setProperty("composant",serviceJson.get("composant").getAsString());
+		properties.setProperty("signature",serviceJson.get("signature").getAsString());
+	}
 
 	@Override
 	public String getServiceName() {
