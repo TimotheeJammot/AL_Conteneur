949d1ca48d755313307bf1764272c16bf72ebe24
Alexandre Lagane <minizarbi@hotmail.fr>
2017-02-10 17:01:13 +0100

    Json parser

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.StringReader;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.*;
+import java.util.Iterator;
 import java.util.Properties;
 
 public class Main {
@@ -10,9 +17,29 @@ public class Main {
         try {
             p1.load(new FileReader("examples/service.properties"));
             System.out.println(p1.toString());
+            System.out.println(p1.getProperty("type"));
+            System.out.println(p1.getProperty("signature"));
 
-            p2.load(new StringReader(p1.toString()));
+            StringWriter sw = new StringWriter();
+            p1.store(sw, null);
+
+            String genStr = sw.toString();
+            p2.load(new StringReader(genStr));
             System.out.println(p2.toString());
+            System.out.println(p2.getProperty("type"));
+            System.out.println(p2.getProperty("signature"));
+
+            FileWriter fw = new FileWriter("examples/service_generated.properties");
+            fw.write(genStr);
+            fw.flush();
+            System.out.println(genStr);
+
+            JsonParser jp = new JsonParser();
+            JsonArray ja = jp.parse(p2.getProperty("signature")).getAsJsonArray();
+            Iterator<JsonElement> ijo = ja.iterator();
+            while (ijo.hasNext()) {
+                System.out.println(ijo.next().getAsJsonObject().get("nom").getAsString());
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
